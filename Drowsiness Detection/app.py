a73fa9389124488c05c2509a84c2d1885949603c
Srihari MOHAN <srihari_mohan@nec.com.sg>
2021-07-21 11:38:48 +0800

    drowsy detection complete

--- Drowsiness Detection/app.py
+++ Drowsiness Detection/app.py
@@ -9,17 +9,14 @@ mp_face_mesh = mp.solutions.face_mesh
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
-thresh = 0.18
+thresh = 0.20
 flag = 0
 frames = 20
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-# org
 org1 = (50, 50)
 org2 = (75,75)
-# fontScale
 fontScale = 1
-# Blue color in BGR
 color1 = (255, 0, 0)
 color2 = (0,0, 255)
 thickness = 2
@@ -31,14 +28,8 @@ with mp_face_mesh.FaceMesh(
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
-
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
     image.flags.writeable = False
     results = face_mesh.process(image)
 
@@ -47,12 +38,6 @@ with mp_face_mesh.FaceMesh(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_face_landmarks:
       for face_landmarks in results.multi_face_landmarks:
-        # mp_drawing.draw_landmarks(
-        #     image=image,
-        #     landmark_list=face_landmarks,
-        #     connections=mp_face_mesh.FACE_CONNECTIONS,
-        #     landmark_drawing_spec=drawing_spec,
-        #     connection_drawing_spec=drawing_spec)
         left_eye_array = [[face_landmarks.landmark[161].x, face_landmarks.landmark[161].y],
                           [face_landmarks.landmark[163].x, face_landmarks.landmark[163].y],
                           [face_landmarks.landmark[157].x, face_landmarks.landmark[157].y],
@@ -73,8 +58,7 @@ with mp_face_mesh.FaceMesh(
             flag += 1
             if(flag >= 20):
                 cv2.putText(image, 'ALERT DROWSY' , org2, font,fontScale, color2, thickness, cv2.LINE_AA)
-                # playsound("")
-                flag = 0
+                playsound(r"assets/alert.wav")
         else:
             flag = 0
     cv2.imshow('MediaPipe FaceMesh', image)
